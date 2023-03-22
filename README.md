910214863e33a8abf3845cca5330c92f7093bb5d
Albin George <albinin0002@gmail.com>
2023-03-22 22:11:37 +0100

    Use IndexDB to store data

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 Lets users add comments on websites
 
+## MVP requirements
+
+- Lets users add comments on websites
+- Data is stored in an IndexDB in browser, meaning data won't be transfered on different devices used by the user
+
+## First release
+
+- Publish addon to Firefox([Reference](https://extensionworkshop.com/documentation/publish/))
+- Storing data in an external service based on user id
+- Supports Google login
+- Syncronisation of data across devices
--- popup/comment.js
+++ popup/comment.js
@@ -1,3 +1,44 @@
+let objectStore;
+let p;
+function init() {
+    // loadBootstrap();
+}
+
+function initDb() {
+    console.log("Init db started");
+    p = new Promise((resolve, reject) => {
+        if (window.db == null) {
+            console.log("initialising db");
+            const request = indexedDB.open("commentsdb", 3);
+            request.onerror = (event) => {
+                console.log("db error");
+                console.error(event.target.error);
+                reject("no indexdb created");
+            };
+            request.onupgradeneeded = (event) => {
+                console.log("onupgradeneeded");
+                window.db = event.target.result;
+                objStore = window.db.createObjectStore("comments", {keyPath: "id", autoIncrement: true});
+                objStore.createIndex("url", "url", { unique: false });
+                resolve(window.db);
+            };
+            request.onsuccess = (event) => {
+                window.db = event.target.result;
+                resolve(window.db);
+                console.log("db success");
+                console.log(window.db);
+            }
+        } else {
+            resolve(window.db);
+        }
+    });
+
+    p.then((db) => {
+        insertComment("My first comment");
+    })
+
+}
+
 function loadBootstrap() {
     var cssId = 'bootstrap-css';
     if (!document.getElementById(cssId))
@@ -13,8 +54,47 @@ function loadBootstrap() {
     }
 }
 
-function fetchComments() {
-    alert("Fetching comments");
+function insertComment(comment) {
+    browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+        let tab = tabs[0];
+        const customer = {url: tab.url, comment: comment};
+        console.log("inserting comment...");
+        console.log(customer);
+
+        const transaction = window.db.transaction("comments", "readwrite")
+
+        // Do something when all the data is added to the database.
+        transaction.oncomplete = (event) => {
+            console.log("transaction complete");
+        };
+
+        transaction.onerror = (event) => {
+            console.log("transaction error");
+            console.log(event.target.error);
+        };
+
+        transaction.onsuccess = (event) => {
+            console.log("db success");
+            window.db = event.target.result;
+            console.log(window.db);
+        }
+
+        const objectStore = transaction.objectStore("comments");
+
+        const request = objectStore.add(customer);
+        request.onsuccess = (event) => {
+            const data = event.target.result;
+            console.log("comment added");
+            console.log("result");
+            console.log(data);
+        };
+    }).catch((error) => {
+        alert("Could not fetch browser tab");
+        console.log(error);
+    });
 }
 
-window.onload=loadBootstrap;
+initDb();
+
+window.onload=init;
+
