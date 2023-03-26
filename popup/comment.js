89d20e71fda66364ae1968a63c7059ae4681bbc2
Albin George <albinin0002@gmail.com>
2023-03-26 22:38:24 +0200

    Implement fetching from IndexDB

--- popup/comment.js
+++ popup/comment.js
@@ -1,42 +1,7 @@
 let objectStore;
-let p;
+let db;
 function init() {
     // loadBootstrap();
-}
-
-function initDb() {
-    console.log("Init db started");
-    p = new Promise((resolve, reject) => {
-        if (window.db == null) {
-            console.log("initialising db");
-            const request = indexedDB.open("commentsdb", 3);
-            request.onerror = (event) => {
-                console.log("db error");
-                console.error(event.target.error);
-                reject("no indexdb created");
-            };
-            request.onupgradeneeded = (event) => {
-                console.log("onupgradeneeded");
-                window.db = event.target.result;
-                objStore = window.db.createObjectStore("comments", {keyPath: "id", autoIncrement: true});
-                objStore.createIndex("url", "url", { unique: false });
-                resolve(window.db);
-            };
-            request.onsuccess = (event) => {
-                window.db = event.target.result;
-                resolve(window.db);
-                console.log("db success");
-                console.log(window.db);
-            }
-        } else {
-            resolve(window.db);
-        }
-    });
-
-    p.then((db) => {
-        insertComment("My first comment");
-    })
-
 }
 
 function loadBootstrap() {
@@ -54,47 +19,123 @@ function loadBootstrap() {
     }
 }
 
-function insertComment(comment) {
+function getDbInstancePromise() {
+    console.log("Init db started");
+    p = new Promise((resolve, reject) => {
+        if (window.db == null) {
+            console.log("initialising db");
+            const request = indexedDB.open("commentsdb", 3);
+            request.onerror = (event) => {
+                console.log("db error");
+                console.error(event.target.error);
+                reject("no indexdb created");
+            };
+
+            // Gets called if the db does not exist
+            request.onupgradeneeded = (event) => {
+                console.log("onupgradeneeded");
+                window.db = event.target.result;
+                objStore = window.db.createObjectStore("comments", {keyPath: "id", autoIncrement: true});
+                objStore.createIndex("url", "url", { unique: false });
+                resolve(window.db);
+            };
+
+            // Return the target result directly if the db already exists
+            request.onsuccess = (event) => {
+                window.db = event.target.result;
+                resolve(window.db);
+                console.log("db success");
+                console.log(window.db);
+            }
+        } else {
+            resolve(window.db);
+        }
+    });
+
+    return p;
+
+}
+
+function insertComment(db, urlValue, commentValue) {
+    const comment = {url: urlValue, comment: commentValue, created: Date.now()};
+    console.log("inserting comment...");
+    console.log(comment);
+
+    const transaction = db.transaction("comments", "readwrite")
+
+    // Do something when all the data is added to the database.
+    transaction.oncomplete = (event) => {
+        console.log("transaction complete");
+    };
+
+    transaction.onerror = (event) => {
+        console.log("transaction error");
+        console.log(event.target.error);
+    };
+
+    transaction.onsuccess = (event) => {
+        console.log("db success");
+    }
+
+    const objectStore = transaction.objectStore("comments");
+
+    const request = objectStore.add(comment);
+    request.onsuccess = (event) => {
+        const data = event.target.result;
+        console.log("comment added");
+        console.log("result");
+        console.log(data);
+    };
+}
+
+function fetchComments(db, url) {
+    const transaction = db.transaction(["comments"]);
+    const objectStore = transaction.objectStore("comments");
+    // const request = objectStore.get(url);
+    request = objectStore.index('url').openCursor(IDBKeyRange.only(url));
+    request.onerror = (event) => {
+    // Handle errors!
+    };
+
+    // https://github.com/mdn/dom-examples/blob/bda3ad1e1ef5c9edf70530cc071bb255024a3aa8/indexeddb-examples/idbkeyrange/scripts/main.js#L156-L170
+    request.onsuccess = (event) => {
+        // Do something with the request.result!
+        // console.log("Fetched results");
+        // console.log(request.result.url);
+        // console.log(event.target.result);
+
+        var cursor = event.target.result;
+        if (cursor) {
+            commentText = cursor.value.comment;
+            commentDate = cursor.value.created;
+
+            console.log("Comment");
+            console.log({id: cursor.value.id, comment: commentText, created: commentDate});
+
+            cursor.continue();
+        } else {
+            console.log("Entries all displayed.");
+        }
+    };
+
+}
+
+p = getDbInstancePromise();
+
+p.then((db) => {
     browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
         let tab = tabs[0];
-        const customer = {url: tab.url, comment: comment};
-        console.log("inserting comment...");
-        console.log(customer);
+        url = tab.url;
 
-        const transaction = window.db.transaction("comments", "readwrite")
+        insertComment(db, url, "My second comment");
 
-        // Do something when all the data is added to the database.
-        transaction.oncomplete = (event) => {
-            console.log("transaction complete");
-        };
-
-        transaction.onerror = (event) => {
-            console.log("transaction error");
-            console.log(event.target.error);
-        };
-
-        transaction.onsuccess = (event) => {
-            console.log("db success");
-            window.db = event.target.result;
-            console.log(window.db);
-        }
-
-        const objectStore = transaction.objectStore("comments");
-
-        const request = objectStore.add(customer);
-        request.onsuccess = (event) => {
-            const data = event.target.result;
-            console.log("comment added");
-            console.log("result");
-            console.log(data);
-        };
+        fetchComments(db, url);
     }).catch((error) => {
         alert("Could not fetch browser tab");
         console.log(error);
     });
-}
 
-initDb();
+})
 
 window.onload=init;
 
